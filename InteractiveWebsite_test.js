7924a2feabf6a7c39700927064b92ba9b50710a7
karste1407 <karste1407@gmail.com>
2015-03-27 23:23:20 +0100

    Update InteractiveWebsite_test.js

--- InteractiveWebsite_test.js
+++ InteractiveWebsite_test.js
@@ -1,16 +1,21 @@
+//Denne funksjonen skal opnes med en gang siden laster
 function events() {
-	var coinOpt_K = document.getElementById("coinOpt_K").innerHTML;
-    var coinOpt_M = document.getElementById("coinOpt_M").innerHTML;
-    var verdict_1 = document.getElementById("verdict_1").innerHTML;
-    var verdict_2 = document.getElementById("verdict_2").innerHTML;
+    var coinOpt_K = document.getElementById("coinOpt_K").innerHTML; //lager en variabel for valget -kron
+    var coinOpt_M = document.getElementById("coinOpt_M").innerHTML; //lager en variabel for valget -mynt
+    var verdict_1 = document.getElementById("verdict_1").innerHTML; //lager en variabel for "du har vunnet" teksten
+    var verdict_2 = document.getElementById("verdict_2").innerHTML; //lager en variabel for "du har tapt" teksten
+    
+    //setter variablene i array'er
     var _list1 = [coinOpt_K, coinOpt_M];
     var _list2 = [verdict_1, verdict_2];
     
+    //finner ut om det faktisk blir kron/mynt
     for (var i = 0; i < _list.length; i++) {
     	_list1[i].addEventListener("click", function() {
     		user_input = this.innerHTML;
     });
     
+    //finner ut om det faktisk blir kron/mynt
     function get_coinState() {
 		var random_int = Math.random();
 		var coin_state = "Error";
@@ -22,7 +27,8 @@ function events() {
 		else if (random_int > 0.5) {
 			coin_state = "Mynt";
 		}
-				
+		
+		//visste ikke hva jeg skulle gjøre hvis Math.random() = 0.5				
 		else {
 			coin_state = "Kron";
 		}
@@ -31,6 +37,7 @@ function events() {
 	}
 }
 
+//legger til en CSS animasjon for å flippe mynt div'en
 function coinFlip_animate() {
 	var css = document.createElement("style");
 	css.type = "text/css";
@@ -38,6 +45,7 @@ function coinFlip_animate() {
 	document.body.appendChild(css);
 }
 
+//hvis det du trykket på er lik resultatet av myntkastet, så vis "You Won!", ellers "You Loose!"
 if (user_input == get_coinState()) {
 	document.getElementById(_list2[0]).style.display = 'block';
 	document.getElementById(_list2[1]).style.display = 'none';
